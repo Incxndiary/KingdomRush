9d35e3828972e4965be65a03974504b2e538bf97
minicreamy <lolzwow619@gmail.com>
2024-11-05 14:28:28 -0500

    New arrow class

---
+++ 0.2v KingdomRushSimulation-Master-copy/Arrow.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Arrow extends Projectile
+{
+    /**
+     * Act - do whatever the Arrow wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    ArrowTower tower = new ArrowTower();
+    double X = tower.getX();
+    double Y = tower.getY();
+    
+    public Arrow(){
+        
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    public void spawn(){
+        addObject(X, Y);
+    }
+}
